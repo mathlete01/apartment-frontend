fbc5b7c9eaa4f1c4dd9e518ffb5ae2de859cffbb
mathlete01 <msallin@gmail.com>
2020-12-24 09:47:31 -0800

    Reverted from reactstrap to Bootstrap

--- src/components/Row_.js
+++ src/components/Row_.js
@@ -1,22 +1,38 @@
 import React, { Component } from "react";
 import RowHeader from "./RowHeader";
 import TileContainer from "./TileContainer";
-// import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-import { Container, Row, Col } from 'reactstrap';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// import { Container, Row, Col } from "reactstrap";
 
 class Row_ extends Component {
+  
+  // filterApartments = () => {
+  //   this.setState({
+  //       filteredApartments: this.props.apartments.filter(
+  //         (apt) => apt.price >= this.props.priceLow && apt.price <= this.props.priceHigh
+  //         ),
+  //     })
+  //     return this.props.filteredApartments
+  // };
+
   render() {
     return (
       <Container fluid className="rowClass">
-      {/* <Container fluid > */}
-        <Row>
+        {/* <Container fluid > */}
+        {/* {this.filterApartments()} */}
+        <Row className="rowClass">
           <Col lg={1}>
             <RowHeader name={this.props.name} count={this.props.count} />
           </Col>
           <Col lg={1}>
-            <TileContainer name={this.props.name} apartments={this.props.apartments} />
+            <TileContainer 
+              name={this.props.name}
+              apartments={this.props.apartments}
+              // apartments={this.props.filteredApartments}
+              // apartments={this.filterApartments()}
+            />
           </Col>
         </Row>
       </Container>
--- src/components/TileContainer.js
+++ src/components/TileContainer.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import Tile from "./Tile";
-// import Container from "react-bootstrap/Container";
-import { Container } from 'reactstrap';
-import { ListGroup, ListGroupItem } from "reactstrap";
+import Container from "react-bootstrap/Container";
+// import { Container } from 'reactstrap';
+// import { ListGroup, ListGroupItem } from "reactstrap";
+import ListGroup from 'react-bootstrap/ListGroup'
 
 class TileContainer extends Component {
   buildTiles = () => {
     return this.props.apartments.map((apartment) => (
-      <ListGroupItem tag="a" href="#">
+      <ListGroup.Item tag="a" href="#">
         <Tile apartment={apartment} className="tiles" />
-      </ListGroupItem>
+      </ListGroup.Item>
     ));
   };
 

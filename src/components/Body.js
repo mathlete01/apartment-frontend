42170deea6522c2f01b0b04deab412f6b5fb4e57
mathlete01 <msallin@gmail.com>
2020-12-23 16:09:06 -0800

    still getting structure set up

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,42 @@
+import React, { Component } from "react";
 import Column from "./Column";
 import RowContainer from "./RowContainer";
 
-const Body = () => {
-  return (
-    <div>
-      Body
-      <Column />
-      <RowContainer />
-    </div>
-  );
-};
+const API = "http://localhost:3000/listings";
+
+class Body extends Component {
+Z
+    state = {
+        listings: [],
+      };
+
+    componentDidMount() {
+        console.log(`* componentDidMount`)
+        fetch(API)
+          .then((res) => res.json())
+          .then((data) => {
+            //? Did not need to make this an anonymous function
+            this.setState({
+              listings: data,
+            });
+          })
+        //   .then((data) => this.renderRows())
+          .then((data) => console.log(data))
+
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      }
+
+  render() {
+    return (
+      <div>
+        Body
+        <Column />
+        <RowContainer listings={this.state.listings}/>
+      </div>
+    );
+  }
+}
 
 export default Body;

072b7861aeab54c6a4c675eaff7fbaea21b2ed45
Steve <stephenvincentibanez@gmail.com>
2020-12-29 11:10:53 -0800

    tile formatting changes, icons on hover

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState } from "react"
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
 import {FaRegThumbsUp} from "react-icons/fa"
@@ -6,21 +7,31 @@ import {FaRegThumbsDown} from "react-icons/fa"
 import {BiRadioCircle} from "react-icons/bi"
 
 const Tile = ({apartment}) => {
+    const [display, setDisplay] = useState("notdisplayed");
+    const showButton = e => {
+        e.preventDefault();
+        setDisplay("displayed");
+    };
+
+    const hideButton = e => {
+        e.preventDefault();
+        setDisplay("notdisplayed");
+    };
 
     return (
-        <Card style={{ width: '30rem' }} className="shadow">
+        <Card style={{ width: '30rem' }} className="shadow" onMouseEnter={e => showButton(e)} onMouseLeave={e => hideButton(e)}>
             <Card.Img className="cardImage" variant="top" src={apartment.images[0].url} />
-            <Container className="d-flex justify-content-between">
-                <span>{apartment.bedrooms} bedrooms</span>
-                <span>{apartment.bathrooms} bathrooms</span>
+            <Container className="d-flex justify-content-between border-bottom">
+                <span>{apartment.bedrooms} bed {apartment.bathrooms} bath</span>
                 <BiRadioCircle/>
-                <span>{apartment.square_feet} square feet</span>
+                <span>{apartment.square_feet} sq. ft.</span>
+                <BiRadioCircle/>
                 <span>${apartment.price}</span>
             </Container>
-            <div className="d-flex justify-content-between">    
-                <FaRegThumbsUp color="green" size="35px"/>
-                <h5 style={{width: '20rem'}}>{apartment.title}</h5>
-                <FaRegThumbsDown color="red" size="35px"/>
+            <div className="d-flex justify-content-between flex-grow-1 align-items-center">    
+                <FaRegThumbsUp color="green" size="35px" className={display}/>
+                <h5>{apartment.title} </h5>
+                <FaRegThumbsDown color="red" size="35px" className={display}/>
             </div>
         </Card>
     );

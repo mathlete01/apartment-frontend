892c661cae5d183ba349a3b7aaa99017fd072779
AlecGrey <alecgrey.coding@gmail.com>
2020-12-29 09:44:19 -0800

    finished component heirarchy

--- src/components/RowContainer.js
+++ src/components/RowContainer.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 
 class RowContainer extends Component {
   buildRow_s = () => {
-    return this.props.neighborhoods.slice(0,3).map((element) => (
+    return this.props.neighborhoods.slice(0,2).map((element) => (
       <TileRow 
       name={element.name} 
       count= {element.apartments.length} 

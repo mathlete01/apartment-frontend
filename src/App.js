5e5ba34739ac542fda36a3b3a9331d164aa9ef82
AlecGrey <alecgrey.coding@gmail.com>
2021-01-06 12:57:34 -0800

    landing page complete with routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-// import React, { Component } from "react";
-// import Header from "./components/Header";
 import Body from "./components/Body";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Landing from './components/Landing';
 import "./App.css";
 
 function App() {
   return (
-    <div id='page' className='d-flex flex-column justify-content-between'>
-      {/* <Header /> */}
-      <Body />
-    </div>
+    <Router>
+      <div id='page' className='d-flex flex-column justify-content-between'>
+        <Route exact path='/' component={ Landing } />
+        <Route exact path='/browse' component={ Body } />
+      </div>
+    </Router>
   );
 }
 

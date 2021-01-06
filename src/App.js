41e19fe34fde90e5c7566cb9977161ed00828201
AlecGrey <alecgrey.coding@gmail.com>
2021-01-06 09:06:35 -0800

    fixed dependency issue with mime

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-// import React, { Component } from "react";
-// import Header from "./components/Header";
 import Body from "./components/Body";
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "./App.css";
 
 function App() {
   return (
     <div id='page' className='d-flex flex-column justify-content-between'>
-      {/* <Header /> */}
       <Body />
     </div>
   );

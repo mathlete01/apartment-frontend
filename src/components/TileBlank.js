b212881d2b757ed19a4ffed5dd90de121920d5f7
AlecGrey <alecgrey.coding@gmail.com>
2021-01-06 17:14:57 -0800

    blank tile good

--- src/components/TileBlank.js
+++ src/components/TileBlank.js
@@ -6,17 +6,8 @@ import Container from 'react-bootstrap/Container'
 const TileBlank = () => {
 
     return (
-        <Card className="tile d-flex justify-content-center align-items-stretch container" >
-            {/* <Carousel className='flex-grow-1'>
-                <Carousel.Item>
-                    <h4 className='cardImage'>Apartment Not found</h4>
-                </Carousel.Item> */}
-            {/* </Carousel> */}
-            <Container className="spec-container d-flex justify-content-between align-items-center border-bottom">
-            </Container>
-            <div className="d-flex justify-content-between align-items-center">
-                <h6 className='card-title' style={{width: '20rem'}}></h6>
-            </div>
+        <Card className='blank'>
+            {/* <div className='test-card'></div> */}
         </Card>
     );
 }
